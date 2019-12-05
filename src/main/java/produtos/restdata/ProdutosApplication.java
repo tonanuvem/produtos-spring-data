c2a279ed61e0e03ab7dadf855c24608d97ad397c
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2019-12-05 12:22:17 -0300

    Update ProdutosApplication.java

--- src/main/java/produtos/restdata/ProdutosApplication.java
+++ src/main/java/produtos/restdata/ProdutosApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProdutosApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProdutosApplication.class, args);
+		SpringApplication.run(ProdutosApplication.class);
 	}
 
 }
