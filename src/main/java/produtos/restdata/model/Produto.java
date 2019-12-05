f99bdef77012c0976dfb1fbd6ae490602093d6e8
Andre Sampaio <andresampaio@MacBook-Pro-de-Andre.local>
2019-12-05 09:58:50 -0300

    criando repo

---
+++ src/main/java/produtos/restdata/model/Produto.java
@@ -0,0 +1,31 @@
+package produtos.restdata.model;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Data; // gera automaticamente: getters, setters, toString(), hashCode()
+
+@Data
+@Entity
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @JsonProperty("nome_produto")   
+    private String nome;
+
+    @JsonProperty("descricao_produto") 
+    private String descricao;
+
+    @JsonProperty("preco_produto") 
+    private float preco;
+}
---
+++ src/test/java/fiap/engdev/restdata/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package fiap.engdev.restdata;
+
+//import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	//@Test
+	void contextLoads() {
+	}
+
+}
